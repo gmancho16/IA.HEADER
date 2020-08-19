43b2a7f96895ad83925719e88db077b788e6e91d
jerry <gmancho16@me.com>
2020-08-19 18:51:59 -0500

    Add files via upload

---
+++ ia.h
@@ -0,0 +1,27 @@
+//variables 
+    int d1 = 0;
+    int d2 = 0;
+    
+    int flag = 0; 
+//fuzzy 
+//contenerdores
+
+    
+
+int fuzz(int c1, int c2){
+
+    //contenedor uno 
+    //printf("esto es de la libreria");
+    
+    if (d1 >= c1){
+        flag = 1;
+    }else{
+        flag = 0;
+    }
+    
+    
+    
+}
+
+//activadores
+//resultados 

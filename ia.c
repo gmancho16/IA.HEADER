ac9ad7cd276f82bb6a0d3681eb8833153336e0dd
jerry <gmancho16@me.com>
2020-09-02 04:13:22 -0500

    Add files via upload

---
+++ ia.c
@@ -0,0 +1,6 @@
+#ifndef _IA_H
+#define  _IA_H
+
+//contenido
+
+#endif 
---
+++ ia.h
@@ -0,0 +1,35 @@
+//variables 
+
+int d1=0;
+int flag=0;
+int c1=0;
+int c2=0;
+
+//fuzzy 
+//contenerdores
+int fuzzy(){
+    
+    if (d1 <= c1){
+        
+        flag = 3;
+        //fria 
+    
+    }
+    else if(d1 >= c2){
+        
+        flag = 1;
+        //calinete
+    }
+    else{   
+        
+        flag = 2;
+        //tibia
+        
+    }
+    
+    
+}
+
+//activadores
+//resultados   
+  
